d4b10f79aa735cd7a9c42ee12722d42b48aa6e48
Aryan Arora <aryanarora.w1@gmail.com>
2022-09-22 13:45:43 +0530

    parse torrentfile

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aryanA101a/villi
 
 go 1.18
+
+require github.com/jackpal/bencode-go v1.0.0
---
+++ torrentfile/torrentfile.go
@@ -0,0 +1,97 @@
+package torrentfile
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"os"
+
+	"github.com/jackpal/bencode-go"
+)
+
+//Por used for Bit-Torrent
+const Port uint16=6881
+
+type TorrentFile struct{
+	Announce string
+	InfoHash [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length int
+	Name string
+}
+
+type bencodeInfo struct{
+	Pieces string `bencode:"pieces"`
+	PieceLength int `bencode:"piece length"`
+	Length int `bencode:"length"`
+	Name string `bencode:"name"`
+}
+
+type bencodeTorrent struct{
+	Announce string `bencode:"announce"`
+	Info bencodeInfo `bencode:"info"`
+}
+
+
+
+func Open(path string) (TorrentFile,error){
+	file,err :=os.Open(path)
+	if err!=nil{
+		return TorrentFile{}, err
+	}
+	defer file.Close()
+
+	bto :=bencodeTorrent{}
+	err=bencode.Unmarshal(file,&bto)
+	if err != nil {
+		return TorrentFile{},err
+	}
+	return bto.toTorrentFile()
+}
+
+func(i *bencodeInfo) hash()([20]byte,error){
+	var buf bytes.Buffer
+	err:= bencode.Marshal(&buf,*i)
+	if err!=nil{
+		return [20]byte{},err
+	}
+	h:=sha1.Sum(buf.Bytes())
+	return h,nil
+}
+
+func (i *bencodeInfo) splitPiecesHashes()([][20]byte,error){
+	hashLen:=20 //Length of SHA1 hash
+	buf:=[]byte(i.Pieces)
+	if len(buf)%hashLen !=0{
+		err:=fmt.Errorf("recieved malformed pieces of length %d",len(buf))
+		return nil,err
+	}
+	numHashes:=len(buf)/hashLen
+	hashes:=make([][20]byte,numHashes)
+
+	for i:=0;i<numHashes;i++{
+		copy(hashes[i][:],buf[i*hashLen:(i+1)*hashLen])
+	}
+	return hashes,nil
+}
+
+func (bto *bencodeTorrent) toTorrentFile()(TorrentFile,error){
+	infoHash,err:=bto.Info.hash()
+	if err!=nil{
+		return TorrentFile{},err
+	}
+	pieceHashes,err:=bto.Info.splitPiecesHashes()
+	if err!=nil{
+		return TorrentFile{}, err
+	}
+	t:= TorrentFile{
+		Announce: bto.Announce,
+		InfoHash: infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:bto.Info.Length,
+		Name:bto.Info.Name,
+	}
+	return t,nil
+}
